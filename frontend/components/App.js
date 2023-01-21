8bf386344d4bf520953e0d12d995b6afeb3f931b
aaschuster <aaronjschuster@gmail.com>
2023-01-20 20:13:03 -0500

    set up basic form

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import Form from "./Form";
+import Todo from "./Todo";
+import TodoList from "./TodoList";
 
 const URL = 'http://localhost:9000/api/todos'
 
+
+
 export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      inputVal: ""
+    }
+  }
+
   render() {
-    return null
+
+    const onChange = (e) => {
+      this.setState({inputVal: e.target.value});
+    };
+    
+    const onSubmit=(e)=> {
+      e.preventDefault();
+      this.setState({inputVal: ""});
+    }
+
+    return(
+      <div>
+        <Form inputVal={this.state.inputVal} onChange={onChange} onSubmit={onSubmit}/>
+        <TodoList />
+      </div>
+    )
   }
 }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    return null
+    return(
+      <form onSubmit={this.props.onSubmit}>
+        <input type="text" value={this.props.inputVal} onChange={this.props.onChange} placeholder="Enter your todo..."/>
+        <button>Submit</button>
+      </form>
+    )
   }
 }
